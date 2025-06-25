7c0add699fc11df6bf57e42715a85fd79e368bad
shafrinahamed097 <shafrinahamedsa@gamil.com>
2025-06-25 02:03:48 -0600

    Display blog data and hashtags

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,9 +1,37 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({blog}) =>{
-    console.log(blog);
+   const {title, cover, reading_time, author, author_img, posted_date, hashtags} = blog;
     return(
         <div>
+            <img src={cover} alt={`Cover picture of the tittle ${title}`} />
+            <div>
+                <div className='flex justify-between'>
+                    <div className='flex'>
+                        <img className='w-14  rounded-full ' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                    </div>
+                   
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+
+
+
+
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+
+            <p>
+               {
+                hashtags.map((hash, idx)=> <span key={idx}><a href="">{hash}</a></span>)
+               }
+
+            </p>
 
         </div>
 
--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
+import PropTypes from 'prop-types';
 
 const Blogs = () =>{
-  const [bolgs, setBlogs] = useState([]);
+  const [blogs, setBlogs] = useState([]);
   useEffect(()=>{
     fetch('blogs.json')
     .then(res=>res.json())
@@ -12,7 +14,16 @@ const Blogs = () =>{
 
     return(
         <div className="md:w-2/3">
-            <h1 className="text-4xl">Blogs: {bolgs.length}</h1>
+            <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+            {
+              blogs.map(blog => <Blog 
+                
+                key={blog.id} 
+                blog={blog}
+                
+                ></Blog>)
+            }
+            
         </div>
     );
 };
